acd4b10ddd5452dcb31e5c92f5588abddcb75d46
HASNAIN-ALI-MUHAMMAD <hasali9349@gmail.com>
2025-04-03 00:46:13 +0500

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
  
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong><br></br> This is where freedom meets flexibility and verstality.<br></br>Join us now!
+            <strong>Welcome to Acme.</strong><br></br> This is where freedom meets you!<br></br>Join us right now!
           </p>
           <Link
             href="/login"
