deb372d298d5bdbf0b2806e128f9b1c19494c47b
HASNAIN-ALI-MUHAMMAD <hasali9349@gmail.com>
2025-04-03 00:25:00 +0500

    About
    NEXTJS practice project

---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import {ABeeZee} from 'next/font/google';
+import {Lusitana} from 'next/font/google';
+export const inter = ABeeZee({subsets: ['latin'], weight: '400'});
+
+export const lusitana = Lusitana({subsets: ['latin'],weight:['400','700']});
